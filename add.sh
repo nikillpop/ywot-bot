dabcc7d5abdce5a6ccddef3be3a95898b2e27a07
Ben Rohrer <benrohrer@DhariRorersiMac.hsd1.ga.comcast.net>
2018-07-31 18:05:38 -0400

    deleted files

--- add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#add a.txt -10 -10 b.txt -3 -3 sum.txt <transparent>; -- transparency is if ampersand overwrites (b.txt is top)
-
-dir="pastes/"
-n=$'\n'
-echo "$1$n$2$n$3$n$4$n$5$n$6$n$8" > dim.txt;
-
-python3 add.py > $7;
--- worldread.sh
+++
@@ -1,6 +0,0 @@
-new=$'\n';
-echo "$1$new$2$new$3$new$4" > dim.txt;
-
-{ sleep 1; echo "{\"fetchRectangles\":[{\"minY\":$1,\"minX\":$2,\"maxY\":$3,\"maxX\":$4}],\"kind\":\"fetch\",\"v\":\"3\"}"; sleep 3; } | wscat -c "ws://www.yourworldoftext.com$5/ws/" > out.txt;
-sed 's/^> //' < out.txt > in.txt; cp in.txt out.txt;
-python3 parse.py > file.txt;
