17b545c1dbe4e269ccc20e54b45be64045fdece8
Holden Rohrer <hdawg7797@yahoo.com>
2018-08-18 11:58:29 -0400

    troll.js and ywot.js updated for efficiency

--- js-lib/troll.js
+++ js-lib/troll.js
@@ -1,32 +1,88 @@
 const ywot = require('./ywot.js');
 
 var client = new ywot.YWOT()
-var main = client.openworld()
+//var worlds = [{'world':client.openworld(),'thissource':'','sourcenotcalled':true, 'name':''}];
+var worlds = [];
+var worldnames = ['','~Seschient'];
+worlds.push.apply(worlds,worldnames.map((name)=>{return {'world':client.openworld(name),'thissource':'','sourcenotcalled':true,'name':name};}));
 
-var thissource;
-var sourcenotcalled = true;
-var emptytile = [];
-for (var x=0; x<16; x++){
-  for (var y=0; y<8; y++){
-    emptytile.push([y,x,' '])
+//var emptytile = [[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ','r','e','k','t',' ','~','h','d','a','w','g','7','7','7',' ']];
+var emptytile = `  this page now |
+  closed to all |
+  future inputs |
+                |
+                |
+                |
+ rekt ~hdawg777 |
+                |`.split('\n').map((row)=>{return row.split('').slice(0,16);});
+var conquer = 0;
+var coordinates = new Set();
+
+function channel(world){
+  return (source)=>{
+    if (world.sourcenotcalled){
+      world.sourcenotcalled = false;
+      world.thissource = source;
+      console.log(world.thissource);
+    }
   }
 }
+worlds.forEach((world)=>{world.world.on('channel', channel(world.world)); console.log(world);});
 
-main.on('channel', (source)=>{
-  if (sourcenotcalled){
-    sourcecalled = false;
-    thissource = source;
+function onopen(world){
+  return () => {
+    world.sourcenotcalled = true;
   }
-});
+}
+worlds.forEach((world)=>{world.world.on('on', onopen(world.world));});
 
-main.on('tileUpdate', (sender, source, tiles)=>{
-  if (sender != thissource){
-    a = [].concat.apply([],Object.keys(tiles).map((tile)=>{
-      return JSON.parse(JSON.stringify(emptytile)).map((char) => {
-        char.unshift(parseInt(tile.split(',')[1]));
-        char.unshift(parseInt(tile.split(',')[0]));
-        return char;}
-      );}))
-    main.write(a);
+function tileUpdate(world){
+  return (sender, source, tiles)=>{
+    coords = Object.keys(tiles);
+    console.log('received ', coords.length, ' tiles\' updates.');
+    if (sender != world.thissource){
+      coordinates.add.apply(coordinates, coords(tiles));
+      conquer += coords(tiles).length;
+      console.log('overwritten ', conquer, ' tiles; overwritten ', coordinates.size, ' unique tiles.');
+      var pretilewrite = coords(tiles).map((tile)=>{
+        return ywot.combine((char1,char2)=>{
+          if (char1 == char2){
+            return '';
+          }
+          else{
+            return char2;
+          }
+        },ywot.tile2space(tiles[tile].content),0,0,emptytile,0,0);
+      });
+      var tilewrite = {}; for (i=0; i<coords(tiles).length; i++){
+        tilewrite[coords(tiles)[i]] = pretilewrite[i];
+      }
+      pretilewrite1 = coords(tilewrite).map((tile) => {
+        newtile = []
+        for (x=0; x<16; x++){
+          for (y=0; y<8; y++){
+            newtile.push([y,x,tilewrite[tile][y][x]]);
+          }
+        }
+        return newtile;
+      });
+      tilewrite = {}; for (i=0; i<coords(tiles).length; i++){
+        tilewrite[coords(tiles)[i]] = pretilewrite1[i];
+      }
+      a = [].concat.apply([],coords(tiles).map((tile)=>{
+        return tilewrite[tile].map((char) => {
+          char.unshift(parseInt(tile.split(',')[1]));
+          char.unshift(parseInt(tile.split(',')[0]));
+          if (char[4] == ''){
+            return '';
+          }
+          else{
+            return char;
+          }
+        }
+      ).filter((char)=>{return char!='';});}));
+      world.write(a);
+    }
   }
-});
+}
+worlds.forEach((world)=>{world.world.on('tileUpdate', tileUpdate(world.world));});
--- js-lib/ywot.js
+++ js-lib/ywot.js
@@ -14,7 +14,7 @@ class YWOT extends EventEmitter{ /*Manages connection frequency with the server*
       else {
         var sock = new ws(`ws://www.yourworldoftext.com/${name}/ws/`);
       }
-      var sock = new ws(`ws://www.yourworldoftext.com/ws/`);
+      //var sock = new ws(`ws://www.yourworldoftext.com/ws/`);
       var world = new World(name, this);
       sock.on('open', () => {
         world.setsock(sock)
@@ -25,16 +25,19 @@ class YWOT extends EventEmitter{ /*Manages connection frequency with the server*
     this.newpush = function(world){
       pushqueue.push(world);
     }
+    this.emptyqueue = function(world){
+      pushqueue = pushqueue.filter(item => {return item == world;});
+      console.log('queue emptied');
+    }
     setInterval(()=>{
       if (pushqueue.length > 0){
-        console.log(pushqueue);
         pushqueue.shift().servpush();
-        console.log(pushqueue.length);
+        console.log('server communications remaining:', pushqueue.length, '; time:', +new Date());
       }
       else{
         this.emit('free');
       }
-    }, 750)
+    }, 800)
   }
 }
 
@@ -47,25 +50,40 @@ class World extends EventEmitter{
     var dimension; //For fetch dimensions
     var dimensions = []; //For fetch dimensions (queue for slow server response)
     var pushqueue = []; //Queue of things to push to server; triggered by YWOT.
+    var writequeue = [];
+    var sockclosed = false;
+    var self = this;
     function newqueue(data,lrg){
       client.newpush(lrg);
       pushqueue.push(data);
     }
+    function retry(e){
+      sockclosed = true;
+      console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+      setTimeout(function() {
+        if ( name === ''){
+          var asock = new ws('ws://www.yourworldoftext.com/ws/');
+        }
+        else {
+          var asock = new ws(`ws://www.yourworldoftext.com/${name}/ws/`);
+        }
+        asock.on('open', () => {
+          sockclosed = false;
+          self.setsock(asock);
+        })
+        asock.onclose = retry;
+        asock.onerror = function(err) {
+          console.error('Socket encountered error: ', err.message, 'Closing socket');
+          sock.close();
+        };
+      }, 1000);
+    }
     this.setsock = function(sockin){
       sock = sockin;
-      ws.onclose = function(e) {
-        console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-        setTimeout(function() {
-          asock = new ws(`ws://www.yourworldoftext.com/ws/`);
-          asock.on('open', () => {
-            this.setsock(asock)
-          })
-        }, 10000);
-      };
-
-      ws.onerror = function(err) {
+      sock.onclose = retry;
+      sock.onerror = function(err) {
         console.error('Socket encountered error: ', err.message, 'Closing socket');
-        ws.close();
+        sock.close();
       };
       sock.on('message', (message) => {
         message = JSON.parse(message);
@@ -103,27 +121,47 @@ class World extends EventEmitter{
       this.emit('on');
     }
     this.servpush = function(){ //Never use outside of YWOT class.
-      sock.send(pushqueue.shift());
-      console.log('servout', callbacks);
+      if (sockclosed){
+        client.newpush(this);
+        return;
+      }
+      var queuetop = pushqueue.shift();
+      if (queuetop === 'USE WRITEQUEUE'){
+        if (writequeue.length < 200){
+          sock.send(formatwrite(writequeue));
+          writequeue = [];
+          client.emptyqueue(this);
+        }
+        else{
+          sock.send(formatwrite(writequeue.splice(0,200)));
+        }
+      }
+      else{
+        sock.send(queuetop);
+      }
     }
-    function unsafewrite(chars,lrg){
+    function formatwrite(chars){
       for (var i=0; i<chars.length; i++){
         chars[i].splice(4,0,0);
         chars[i].push(i);
       }
-      newqueue(`{"edits":${JSON.stringify(chars)},"kind":"write"}`, lrg);
+      return `{"edits":${JSON.stringify(chars)},"kind":"write"}`;
     }
     this.write = function(chars){ //tileY, tileX, charY, charX, char
-      console.log(chars);
       var batch = [];
       for (var i=0; i<chars.length; i++){
         if (batch.length == 200){
-          unsafewrite(batch,this);
+          writequeue.push.apply(writequeue,batch);
+          newqueue('USE WRITEQUEUE', this);
           batch = [];
         }
-        batch.push(chars[i])
+        batch.push(chars[i]);
       }
-      unsafewrite(batch,this);
+      writequeue.push.apply(writequeue,batch);
+      newqueue('USE WRITEQUEUE', this);
+      writequeue = writequeue.filter(function(item, pos) {
+        return writequeue.indexOf(item) == pos;
+      })
     }
     this.fetch = function(coords, call){
       console.log('fetch request added');
@@ -163,10 +201,62 @@ function fetch2space(fetch,dimension){
    }
    return space;
 }
+exports.fetch2space = fetch2space;
 
-function combine(charcomb, space1, space2){ //charcomb is character combinator (a function which combines two characters into a result); spaces are two strings which are combined
+function tile2space(tile){
+  rows = [[],[],[],[],[],[],[],[]];
+  for (x=0; x<16; x++){
+    for (y=0; y<8; y++){
+      rows[y].push(tile.split('')[y*16+x])
+    }
+  }
+  return rows;
+}
+exports.tile2space = tile2space;
+
+function combine(charcomb, space1, x1, y1, space2, x2, y2){ //charcomb is character combinator (a function which combines two characters into a result); spaces are two strings which are combined
+  var lcol = Math.min(x1,x2); var ucol = Math.max(Math.max.apply(null,space1.map((row)=>{return row.length;}))+x1,Math.max.apply(null,space2.map((row)=>{return row.length;}))+x2);
+  var newspace = [];
+  for (var row=Math.min(y1,y2); row<Math.max(space1.length+y1, space2.length+y2); row++){
+    var newrow = []
+    for (var col=lcol; col<ucol; col++){
+      try{
+        var char1 = space1[row][col];
+      }
+      catch(err){
+        var char1 = '';
+      }
+      try{
+        var char2 = space2[row][col];
+      }
+      catch(err){
+        var char2 = '';
+      }
+      newrow.push(charcomb(char1,char2));
+    }
+    newspace.push(newrow);
+  }
+  return newspace;
+}
+exports.combine = combine;
+
+function file2space(filename){
+  data = fs.readFile(filename, (err)=>{console.log(err);});
+  return data.split('\n').map((row)=>{return row.split('');});
+}
+exports.file2space = file2space;
+
+function space2file(filename, space){
+  data = space.map((row)=>{return row.join('');});
+  fs.writeFile(filename, data.join('\n'));
+}
+exports.space2file = space2file;
+
+function space2tiles(space){
+
 
 }
+exports.space2tiles = space2tiles;
 
 exports.YWOT = YWOT;
 exports.World = World;
